223d95ecab69201cee1efa107fca19cf2eee4434
OptiPhoenix <optiphoenix@OptiPhoenixs-MacBook-Air.local>
2021-07-21 19:49:09 +0530

    final commit

--- v2/smc-redesign.js
+++ v2/smc-redesign.js
@@ -104,16 +104,18 @@
         var filterLinkHTML = '<div class="custom-recipe-filter-link-container"><button class="custom-recipe-filter-link">Got allergies or dietary requirements?</button></div>';
         document.querySelector('.sticky-outer-wrapper').insertAdjacentHTML('afterend', filterLinkHTML);
         document.querySelector('.custom-recipe-filter-link').addEventListener('click', function () {
-            document.querySelector('header + .container-fluid>div:nth-of-type(2) button:nth-of-type(1)').click();
+            document.querySelector('header + .container-fluid div:nth-of-type(3)>button.flex-center').click(); 
         });
     }
 
     function veganLabel() {
         document.querySelectorAll('.card-body div .card-text').forEach(function (label) {
-           if (label.textContent.trim().toLowerCase() === "vegan" || label.textContent.trim().toLowerCase() === "vegetarian") {
-               label.classList.add("custom-veg-label");
-           } 
-        });
+            if (label.textContent.trim().toLowerCase() === "vegan" || label.textContent.trim().toLowerCase() === "vegetarian") {
+                label.classList.remove("custom-veg-label");
+            } else {
+                label.classList.add("custom-veg-label");
+            }
+         });
     }
 
     function recipeCtaChanges() {
